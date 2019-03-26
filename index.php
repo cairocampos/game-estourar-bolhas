0358b02fae2aa52b8786e9d502641e4052e9e963
Cairo Campos <cairocampos98@gmail.com>
2019-03-26 10:34:45 -0300

    Adicionando placar no game e deixando as cores das bolhas dinamicas

--- index.php
+++ index.php
@@ -6,7 +6,10 @@
 		<link rel="stylesheet" href="css/style.css">
 	</head>
 	<body onload="start();">
-		
+		<div id="placar">
+			<h1>PONTOS</h1>
+			<span id="pontos">0</span>
+		</div>
 
 		<script type="text/javascript" src="js/script.js"></script>
 	</body>
--- js/script.js
+++ js/script.js
@@ -8,8 +8,13 @@ function load()
 	bolha.setAttribute("class", "bolha");
 	bolha.setAttribute("onclick", "estourar(this)");
 
-	var pos1 = Math.floor(Math.random() * 800);
+	var pos1 = Math.floor(Math.random() * 1200);
 	var pos2 = Math.floor(Math.random() * 500);
+
+	var cores = ["yellow", "red", "blue", "green", "orange"];
+
+	var cor = Math.floor(Math.random() * cores.length);
+	bolha.style.backgroundColor = cores[cor];	
 
 	bolha.style.marginLeft = pos1 + "px";
 	bolha.style.marginTop = pos2 + "px";
@@ -21,4 +26,11 @@ function estourar(obj)
 {
 	var elemento = obj.parentNode;
 	elemento.removeChild(obj);
+
+	atualizarPlacar();
+}
+
+function atualizarPlacar()
+{
+	document.getElementById("pontos").innerHTML++;
 }
